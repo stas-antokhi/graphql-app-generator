18526fad60245f373e8228feae4d39c97e928e4f
stas-antokhi <stas@softinstigate.com>
2021-10-20 23:53:41 +0200

    Changed app name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'grapgql-app-creator';
+  title = 'grapgql-app-generator';
 }
