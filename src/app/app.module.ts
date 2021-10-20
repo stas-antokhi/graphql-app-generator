0a5b4e66d00257d8ba200eadd20ae71587def148
stas-antokhi <stas@softinstigate.com>
2021-10-20 23:51:05 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatButtonModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ tailwind.config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  important: true,
+  purge: ["./src/**/*.html", "./src/**/*.ts"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          '100': '#72c1df',
+          '200': '#5bb7d9',
+          '300': '#43add4',
+          '400': '#2ca2ce',
+          '500': '#1498c9',
+          '600': '#1289b5',
+          '700': '#107aa1',
+          '800': '#0e6a8d',
+          '900': '#0c5b79'
+        }
+      },
+      fontFamily: {
+        'primary': ['Crimson Text', 'serif'],
+        'secondary': ['Amiko', 'sans-serif']
+      }
+    },
+    // adapted angular material box-shadow
+    boxShadow: {
+      //        offset-x | offset-y | blur-radius | spread-radius | color
+      DEFAULT: '0px 3px 3px -2px rgb(0 0 0 / 20%), 0px 2px 2px 0px rgb(0 0 0 / 14%), 0px 1px 5px 0px rgb(0 0 0 / 12%)',
+      'lg': '3px 4px 2px 0px rgb(0 0 0 / 20%), 0px 2px 2px 0px rgb(0 0 0 / 14%), 0px 1px 5px 0px rgb(0 0 0 / 12%)',
+      none: 'none'
+    },
+    colors: {
+      accent: {
+        '500': '#4C956C'
+      }
+    },
+    // adapted to angular material breakpoints
+    screens: {
+      sm: '600px',
+      md: '960px',
+      lg: '1280px',
+      xl: '1920px'
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
