f84b800b7c10af766273b4545e19e8c55b8c5328
stas-antokhi <stas@softinstigate.com>
2021-10-30 14:48:26 +0200

    Created a component to represent the descriptor of a graphql app

--- src/app/components/graphql/graphql-descriptor/graphql-descriptor.component.ts
+++ src/app/components/graphql/graphql-descriptor/graphql-descriptor.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-graphql-descriptor',
   templateUrl: './graphql-descriptor.component.html',
   styleUrls: ['./graphql-descriptor.component.scss']
 })
-export class GraphqlDescriptorComponent implements OnInit {
+export class GraphqlDescriptorComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  @Output() validForm = new EventEmitter<boolean>();
+
+  descriptorForm!: FormGroup;
+
+  statusSub!: Subscription;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.descriptorForm = this.fb.group({
+      description: [''],
+      uri: ['', Validators.required],
+      name: ['', Validators.required],
+      enabled: [true]
+    });
+
+    this.statusSub = this.descriptorForm.statusChanges.subscribe(
+      status => this.validForm.emit(status === 'VALID')
+    );
+  }
+
+  ngOnDestroy() {
+    this.statusSub.unsubscribe();
   }
 
 }
