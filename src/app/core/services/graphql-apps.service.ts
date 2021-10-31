c00ce310bd29a56f057c6c7a7177cb360fc5e95f
stas-antokhi <stas@softinstigate.com>
2021-10-31 18:03:23 +0100

    Added function to create graphql app

--- src/app/core/services/graphql-apps.service.ts
+++ src/app/core/services/graphql-apps.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Graphql } from 'src/app/shared/models/GraphQLApp';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +9,18 @@ import { Observable } from 'rxjs';
 export class GraphqlAppsService {
 
   private mockURL = 'http://localhost:8080/gql-apps';
+  headers = {'Authorization': 'Basic ' + btoa('admin:secret')}
 
   constructor(private http: HttpClient) { }
 
   getAllApps(): Observable<any> {
-    return this.http.get(this.mockURL, { headers: {'Authorization': 'Basic ' + btoa('admin:secret')}});
+    return this.http.get(this.mockURL, { headers: this.headers});
+  }
+
+  createApp(data: Graphql.App): Observable<HttpResponse<any>> {
+    return this.http.post(this.mockURL, data, {
+      observe: 'response',
+      headers: this.headers
+    });
   }
 }
