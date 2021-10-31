c0fd8ad659f986e82246dd36a701e7d363dfe520
stas-antokhi <stas@softinstigate.com>
2021-10-31 18:05:39 +0100

    Added interface to represent monaco Model Markers (used for warning/errors)

--- src/app/shared/models/json-schema.ts
+++ src/app/shared/models/json-schema.ts
@@ -4,3 +4,17 @@ export interface JsonSchema {
   fileMatch?: [];
 }
 
+export interface ModelMarker {
+  code: any | undefined;
+  endColumn: number;
+  endLineNumber: number;
+  message: string;
+  owner: string;
+  relatedInformation: undefined | any;
+  resource: any;
+  severity: number;
+  source: undefined | any;
+  startColumn: number;
+  startLineNumber: number;
+  tags: undefined | any;
+}
